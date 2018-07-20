a6e61cf74f40be1ddf710dc8b18f8f4f8910c8f3
Kyle <kyle@Kyles-MacBook-Pro.local>
2018-07-19 17:35:43 -0700

    defining app core logic

---
+++ src/App.spec.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Enzyme from 'enzyme';
+
+import App from './App.js';
+
+describe('general requirements', ()=>{
+    it('Must be written in react', ()=>{
+    })
+    it("shouldn't crash", ()=>{});
+})
+
+describe('form features', ()=>{
+    it('should include a firstName field', ()=>{});
+    it('should include a lastName field', ()=>{});
+    it('should include a score field', ()=>{});
+    it('should update the scoreboard when it submits', ()=>{});
+
+    // In a real-world scenario, we would have an integration test 
+    // to make sure the backend contract is stable
+})
